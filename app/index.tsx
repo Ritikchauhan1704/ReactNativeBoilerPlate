ae29d5fe0aa1eb18940501102d2553671e5cd7e4
Ritik Chauhan <critik1704@gmail.com>
2024-12-30 00:22:02 +0530

    hi

---
+++ app/index.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native'
+
+export default function Index() {
+  return (
+    <View className="flex h-screen w-full justify-center items-center">
+      <Text>Hello world</Text>
+    </View>
+  )
+}
